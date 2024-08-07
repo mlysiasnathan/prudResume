341c6b84ffb24c121e05505ce3c322e775cd2acf
LysNBrain <mlysiasnathan@gmail.com>
2024-08-07 13:41:16 +0200

    get in touch done

--- public/assets/vendor/php-email-form/validate.js
+++ public/assets/vendor/php-email-form/validate.js
@@ -14,9 +14,9 @@
 
       let thisForm = this;
 
-      let action = thisForm.getAttribute('action');
       let recaptcha = thisForm.getAttribute('data-recaptcha-site-key');
-      
+      let action = "https://formsubmit.co/ajax/lyadungamigaboprudent@gmail.com";
+
       if( ! action ) {
         displayError(thisForm, 'The form action property is not set!')
         return;
@@ -24,6 +24,7 @@
       thisForm.querySelector('.loading').classList.add('d-block');
       thisForm.querySelector('.error-message').classList.remove('d-block');
       thisForm.querySelector('.sent-message').classList.remove('d-block');
+      thisForm.querySelector('.submit-btn').classList.add('d-none');
 
       let formData = new FormData( thisForm );
 
@@ -32,10 +33,10 @@
           grecaptcha.ready(function() {
             try {
               grecaptcha.execute(recaptcha, {action: 'php_email_form_submit'})
-              .then(token => {
-                formData.set('recaptcha-response', token);
-                php_email_form_submit(thisForm, action, formData);
-              })
+                  .then(token => {
+                    formData.set('recaptcha-response', token);
+                    php_email_form_submit(thisForm, action, formData);
+                  })
             } catch(error) {
               displayError(thisForm, error)
             }
@@ -50,36 +51,43 @@
   });
 
   function php_email_form_submit(thisForm, action, formData) {
+
     fetch(action, {
-      method: 'POST',
-      body: formData,
-      headers: {'X-Requested-With': 'XMLHttpRequest'}
+      method: "POST",
+      body: JSON.stringify({
+        Names: formData.get('name'),
+        email: formData.get('email'),
+        Subject: formData.get('subject'),
+        Message: formData.get('message'),
+        _captcha: true,
+        _autoresponse : "Thank you Dear customer for getting in touch. I will answer very soon",
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
     })
-    .then(response => {
-      if( response.ok ) {
-        return response.text()
-      } else {
-        throw new Error(`${response.status} ${response.statusText} ${response.url}`); 
-      }
-    })
-    .then(data => {
-      thisForm.querySelector('.loading').classList.remove('d-block');
-      if (data.trim() == 'OK') {
-        thisForm.querySelector('.sent-message').classList.add('d-block');
-        thisForm.reset(); 
-      } else {
-        throw new Error(data ? data : 'Form submission failed and no error message returned from: ' + action); 
-      }
-    })
-    .catch((error) => {
-      // displayError(thisForm, error);
-    });
+        .then(response => response.json())
+        .then(data => {
+          let msg = "This form needs Activation. We\'ve sent you an email containing an \'Activate Form\' link. Just click it and your form will be actived!"
+          let msg2 = "The form was submitted successfully."
+          thisForm.querySelector('.loading').classList.remove('d-block');
+          if (data.success === true || data.message === msg || data.message === msg2) {
+            thisForm.querySelector('.sent-message').classList.add('d-block');
+            thisForm.querySelector('.submit-btn').classList.remove('d-none');
+            thisForm.reset();
+          } else {
+            throw new Error(data ? data.message : 'Form submission failed and no error message returned from: ' + action);
+          }
+        })
+        .catch(error => displayError(thisForm, error));
   }
 
   function displayError(thisForm, error) {
     thisForm.querySelector('.loading').classList.remove('d-block');
     thisForm.querySelector('.error-message').innerHTML = error;
     thisForm.querySelector('.error-message').classList.add('d-block');
+    thisForm.querySelector('.submit-btn').classList.remove('d-none');
   }
 
 })();
